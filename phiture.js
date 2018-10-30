613a1900bdd749f7cb05f1a970153677685b577c
alexandre gorius <ihkr@orange.fr>
2018-10-30 16:37:51 +0100

    make my profile prettier

--- phiture.js
+++ phiture.js
@@ -4,13 +4,13 @@ var phiture = {
   // Adds the platfrom classes to the body tag
   addPlatformClassName:  function () {
     let isDev = location.search.indexOf('dev=true') > -1; // is this a dev
-    let isAndroid = window.navigator.userAgent.toLowerCase().indexOf('android') > -1;// Is this an android phone 
-    let body = document.body; // Get the body tag  
-    body.classList.add(isAndroid ? 'android' : 'ios'); 
+    let isAndroid = window.navigator.userAgent.toLowerCase().indexOf('android') > -1;// Is this an android phone
+    let body = document.body; // Get the body tag
+    body.classList.add(isAndroid ? 'android' : 'ios');
     body.classList.toggle('dev', isDev);
   },
 
-  // Tag the event function 
+  // Tag the event function
   tagEvent: function (eventName, properties) {
     var payload = [];
     Object.keys(properties).forEach(function (key) {
@@ -22,6 +22,6 @@ var phiture = {
 
   // Closes the message to the server
   closeMessage: function () {
-      location.href = 'appboy://close';
+      location.href = 'http://leanplum:close';
   }
-} 
+}
